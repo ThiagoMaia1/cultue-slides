a8214717da13dd1b4c136858f0aad44cbfeb2d87
ThiagoMaia1 <tthiagopmaia@gmail.com>
2021-02-27 22:34:40 -0300

    Esc funcionar no popup mesmo no input
    Former-commit-id: 7c00922937623df6cebca56d113813733d682449
    Former-commit-id: c9f273395010475e93cc9debaea617b842686f73

--- src/Components/Popup/Popup.jsx
+++ src/Components/Popup/Popup.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
+import { useHotkeysFilter } from '../../principais/atalhos';
 import './style.css';
 
-class Popup extends React.Component {
+const Popup = ({tamanho, ocultarPopup, children, style}) => {
   
-  constructor (props) {
-    super(props);
-    if (props.tamanho !== 'pequeno') this.tamanho = {height: '65%', width: '50%'};
-  }
+  useHotkeysFilter((e, resultadoFiltroInicial) => {
+    return e.key === 'Escape' || resultadoFiltroInicial;
+  })
 
-  render() {
-    return (
-      <>
-        <div id="fundo-popup" onClick={() => this.props.ocultarPopup(-1)}>
-          <div className='popup' onClick={(e) => e.stopPropagation()} style={{...this.tamanho, ...this.props.style}}>
-            <button id='fechar' onClick={() => this.props.ocultarPopup(-1)}>✕</button>
-            {this.props.children}
-          </div>
+  let estiloTamanho = {};
+  if (tamanho !== 'pequeno')
+    estiloTamanho = {height: '65%', width: '50%'};
+
+  return (
+    <>
+      <div id="fundo-popup" onClick={() => ocultarPopup(-1)}>
+        <div className='popup' onClick={(e) => e.stopPropagation()} style={{...estiloTamanho, ...style}}>
+          <button id='fechar' onClick={() => ocultarPopup(-1)}>✕</button>
+          {children}
         </div>
-      </>
-    );
-  }
-};
+      </div>
+    </>
+  );
+}
   
 export default Popup;
   
